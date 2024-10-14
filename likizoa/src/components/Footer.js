20a34842f18ba48745973f1f8fbc38e95d741d80
Gaseines <gaseinesdzn@hotmail.com>
2024-10-14 19:12:37 -0300

    Updates

--- likizoa/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './Footer.module.css'
-
-function Footer(){
-    return <footer>&copy; Developer Gabriel Nunes. Todos os direitos reservados.</footer>
-}
-
-export default Footer
