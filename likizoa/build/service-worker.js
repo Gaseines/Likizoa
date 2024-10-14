e2a667743ff8859ca5b3da15e70b51565e62612b
Gaseines <gaseinesdzn@hotmail.com>
2024-10-14 20:10:42 -0300

    link ajustado

--- likizoa/build/service-worker.js
+++ likizoa/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0db483d66fb2c2fc9a064feec53f81e.js"
+  "/Likizoa/precache-manifest.ee90c5cad50923fee39d5741ac6d1bbf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Likizoa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
