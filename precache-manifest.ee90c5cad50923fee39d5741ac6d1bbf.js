37095ba551adcd2e34d88ad7a4d0137d057ad57c
Gaseines <gaseinesdzn@hotmail.com>
2024-10-14 20:04:37 -0300

    Updates

---
+++ precache-manifest.ee90c5cad50923fee39d5741ac6d1bbf.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43e07ff28999539092356b76b9e9f6d",
+    "url": "/Likizoa/index.html"
+  },
+  {
+    "revision": "6c01625d6cb463c0191e",
+    "url": "/Likizoa/static/css/main.5e181305.chunk.css"
+  },
+  {
+    "revision": "6662b4360e6229ecd540",
+    "url": "/Likizoa/static/js/2.25114277.chunk.js"
+  },
+  {
+    "revision": "13951c761dd7f214cdc0b4ee85c784dd",
+    "url": "/Likizoa/static/js/2.25114277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde245e6967bb4c8dfea",
+    "url": "/Likizoa/static/js/3.dd1d7f4b.chunk.js"
+  },
+  {
+    "revision": "6c01625d6cb463c0191e",
+    "url": "/Likizoa/static/js/main.9d099980.chunk.js"
+  },
+  {
+    "revision": "e69ba768334de9e530ee",
+    "url": "/Likizoa/static/js/runtime-main.9a2d7bed.js"
+  },
+  {
+    "revision": "cc536fce18de507445a375d9b01a8c0c",
+    "url": "/Likizoa/static/media/BC.cc536fce.png"
+  },
+  {
+    "revision": "54677b2cd32080e556f71d1e4c356500",
+    "url": "/Likizoa/static/media/Barcellos.54677b2c.png"
+  },
+  {
+    "revision": "60cd76b2ef42ff693297c5fc40d25380",
+    "url": "/Likizoa/static/media/Baroncello.60cd76b2.png"
+  },
+  {
+    "revision": "7acea00f362e5232ddb1e57963fc0be9",
+    "url": "/Likizoa/static/media/Betel.7acea00f.png"
+  },
+  {
+    "revision": "460452e0f3efc35b7a463809bfdabed1",
+    "url": "/Likizoa/static/media/Beviani.460452e0.png"
+  },
+  {
+    "revision": "86005759bae8c7727cbb9d1c1b5bccbe",
+    "url": "/Likizoa/static/media/CODE.86005759.png"
+  },
+  {
+    "revision": "b9e671457db40571268b05dae1fe27b8",
+    "url": "/Likizoa/static/media/ComexCargo.b9e67145.png"
+  },
+  {
+    "revision": "bbaa196174939fb07c1b6482d89e3b16",
+    "url": "/Likizoa/static/media/Contrans.bbaa1961.png"
+  },
+  {
+    "revision": "3cee3430fbbe398a0221ef0aada23331",
+    "url": "/Likizoa/static/media/Dumaszak.3cee3430.png"
+  },
+  {
+    "revision": "3da34dc03a01c5e090f3dfb6dd086106",
+    "url": "/Likizoa/static/media/EIL.3da34dc0.png"
+  },
+  {
+    "revision": "08408672e7ae0058d9fb2398ff3b038a",
+    "url": "/Likizoa/static/media/Froes.08408672.png"
+  },
+  {
+    "revision": "25e9bf72183777e532853cca93d89574",
+    "url": "/Likizoa/static/media/GTL.25e9bf72.png"
+  },
+  {
+    "revision": "342cc86862b7ed5311fcc4308f1b0b6f",
+    "url": "/Likizoa/static/media/Jomar.342cc868.png"
+  },
+  {
+    "revision": "ec267286b34279fe2f828f01ac18f1b0",
+    "url": "/Likizoa/static/media/Kobrasol.ec267286.png"
+  },
+  {
+    "revision": "1d5eb2c448c43c6a0cdbe42edc91ee92",
+    "url": "/Likizoa/static/media/MGE.1d5eb2c4.png"
+  },
+  {
+    "revision": "818e67935c5bffe5801f8ed8d3d05729",
+    "url": "/Likizoa/static/media/MWM.818e6793.png"
+  },
+  {
+    "revision": "0a036fd57673b68754c60084769124ba",
+    "url": "/Likizoa/static/media/Nardi.0a036fd5.png"
+  },
+  {
+    "revision": "a24ceafb6d8e8f3d29e83a5a7b197a5f",
+    "url": "/Likizoa/static/media/PTC.a24ceafb.png"
+  },
+  {
+    "revision": "1553ba25cc7991b252f7b07a601388b2",
+    "url": "/Likizoa/static/media/Portolog.1553ba25.png"
+  },
+  {
+    "revision": "e18ec7c7f65c6183a4fcce9d7fe5f3d8",
+    "url": "/Likizoa/static/media/RTM.e18ec7c7.png"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/Likizoa/static/media/Roboto-Regular.5673da52.ttf"
+  },
+  {
+    "revision": "6ec71a243d72fa52ec78291cba61f4fd",
+    "url": "/Likizoa/static/media/Saff.6ec71a24.png"
+  },
+  {
+    "revision": "5748c7e80de326f99292e24a85ac917e",
+    "url": "/Likizoa/static/media/Sanmartino.5748c7e8.png"
+  },
+  {
+    "revision": "8e7a2b8154700e92e9ce880b87e3fe70",
+    "url": "/Likizoa/static/media/SemFronteiras.8e7a2b81.png"
+  },
+  {
+    "revision": "ab1c63ae61104921cd4e29b693c65b83",
+    "url": "/Likizoa/static/media/Simas.ab1c63ae.png"
+  },
+  {
+    "revision": "8e55762109712dda52302b3fd358f729",
+    "url": "/Likizoa/static/media/Spiazzi.8e557621.png"
+  },
+  {
+    "revision": "d104bbaa20688fc8c245f3a1d82106da",
+    "url": "/Likizoa/static/media/Tac.d104bbaa.png"
+  },
+  {
+    "revision": "157b6d9ed3347d37e48215414b2977a6",
+    "url": "/Likizoa/static/media/Vibelog.157b6d9e.png"
+  },
+  {
+    "revision": "960ae55e50d760a37688919ecc9c5358",
+    "url": "/Likizoa/static/media/caminhao_sol.960ae55e.jpg"
+  },
+  {
+    "revision": "664bd97038b714b2f0dd8ffa7297c387",
+    "url": "/Likizoa/static/media/capa_home.664bd970.jpg"
+  },
+  {
+    "revision": "0faa3dfd49e34343d1f3111452d623fd",
+    "url": "/Likizoa/static/media/capa_jornada.0faa3dfd.png"
+  },
+  {
+    "revision": "3a039edde93ad79e42c722f1e64ba252",
+    "url": "/Likizoa/static/media/coolvetica.3a039edd.otf"
+  },
+  {
+    "revision": "4c270a913f4b6b99dde68828b47fd418",
+    "url": "/Likizoa/static/media/fundo_com_linhas.4c270a91.jpg"
+  },
+  {
+    "revision": "c41e609225cec4e776319636b25637a6",
+    "url": "/Likizoa/static/media/gh.c41e6092.png"
+  },
+  {
+    "revision": "a2ca8e241f06e46c0a8c0df2b86a36ba",
+    "url": "/Likizoa/static/media/lfCargo.a2ca8e24.png"
+  },
+  {
+    "revision": "fc0760dffe0c06ac42a08041fbdbe02c",
+    "url": "/Likizoa/static/media/logo.fc0760df.png"
+  },
+  {
+    "revision": "6079cbef51c8a5069db1b056b314975a",
+    "url": "/Likizoa/static/media/notbook.6079cbef.jpg"
+  },
+  {
+    "revision": "58b2a5fb3053c9d9610d3ff6630d3447",
+    "url": "/Likizoa/static/media/portoex.58b2a5fb.png"
+  },
+  {
+    "revision": "1d2ee9a104372f72535275f171a26407",
+    "url": "/Likizoa/static/media/smLog.1d2ee9a1.png"
+  },
+  {
+    "revision": "3375d9c36683c3c0cf27868352854525",
+    "url": "/Likizoa/static/media/transcosta.3375d9c3.png"
+  },
+  {
+    "revision": "0e1a207f7fcc60689c84e68c9c1624f0",
+    "url": "/Likizoa/static/media/transmoor.0e1a207f.png"
+  }
+]);
