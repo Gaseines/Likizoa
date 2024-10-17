116465dfbeafa29168e5180dad284d081916a7fc
Gaseines <gaseinesdzn@hotmail.com>
2024-10-17 18:08:14 -0300

    cor hamburguer

--- likizoa/src/components/MenuHome.js
+++ likizoa/src/components/MenuHome.js
@@ -134,7 +134,7 @@ function MenuHome() {
                 <a href="#welcome">
                     <img className={styles.img_logo} src={logo} alt="Likizoa" />
                 </a>
-                <svg ref={burguerRef} className={burguerClass} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+                <svg ref={burguerRef} className={burguerClass} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffffff" fill="none">
                     <path d="M4 5L20 5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M4 12L20 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M4 19L20 19" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
