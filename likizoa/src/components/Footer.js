d1f5d7b6720bb7cbd43f97035fe362527efdbdd5
Gaseines <gaseinesdzn@hotmail.com>
2024-10-31 11:47:25 -0300

    link footer

--- likizoa/src/components/Footer.js
+++ likizoa/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Footer.module.css'
 
 function Footer(){
-    return <footer>&copy; Developer Gabriel Nunes. Todos os direitos reservados.</footer>
+    return <footer>&copy; Developer <a className={styles.footer_link} target='_blank' href="https://gaseines.github.io/Portifolio/">Gabriel Nunes</a>. Todos os direitos reservados.</footer>
 }
 
 export default Footer
